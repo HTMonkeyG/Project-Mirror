4d6ce0c512b62ba6175619aa8dfd0c4e488a86a8
HTMonkeyG <3254339747@qq.com>
2024-09-27 20:39:21 +0800

    Optimized enchantment seed cracker

--- Enchantment/EnchantmentSeedCracker/EnchantmentSeedCracker.js
+++ Enchantment/EnchantmentSeedCracker/EnchantmentSeedCracker.js
@@ -5,56 +5,117 @@ const { MT } = require("../../Utils/RandomSource.js")
   , Worker = wt.Worker;
 Worker.prototype.addEventListener = Worker.prototype.on;
 
+var workers = []
+  , completed = 0
+  , average = 0
+  , seeds = []
+  , maxThread = Math.max(os.cpus().length - 1, 1);
+  
+maxThread = 8;
+
+console.log(maxThread)
+
+for (workers = [], i = 0; i < maxThread; i++)
+  workers[i] = new Worker(pl.join(__dirname, "./Worker.js"));
+
 function firstInput(costs, effectiveBlock) {
   return new Promise(function (res, rej) {
-    var workers = []
-      , completed = 0
-      , average = 0
-      , seeds = []
-      , maxThread = Math.max(os.cpus().length - 1, 1);
-
-    for (workers = [], i = 0; i < maxThread; i++)
-      workers[i] = new Worker(pl.join(__dirname, "./Worker.js"));
-
+    completed = 0;
+    average = 0;
+    seeds = [];
     workers.forEach(function (a, b) {
       a.addEventListener("message", function (e) {
+        if (completed >= maxThread)
+          return;
         seeds = seeds.concat(e.data.seeds)
         completed++;
         average += e.data.t;
         if (completed >= maxThread) {
+          console.log(0);
           average /= maxThread;
           res({
             seeds: seeds,
             avrTime: average
           })
         }
-        a.terminate();
       }, !1)
     });
 
-    var m = (2 ** 32)
+    var m = (2 ** 24)
       , blockLength = Math.floor(m / maxThread)
       , tail = m % blockLength;
-    for (var i = 0, j = 0; j < maxThread - 1; i += blockLength, j++) {
-      workers[j].postMessage({
-        blockLength: blockLength,
-        costs: costs,
-        minSeed: i,
-        effectiveBlock: effectiveBlock
-      });
-    }
+    if(maxThread > 1)
+      for (var i = 0, j = 0; j < maxThread - 1; i += blockLength, j++) {
+        workers[j].postMessage({
+          type: "first",
+          blockLength: blockLength,
+          costs: costs,
+          minSeed: i,
+          effectiveBlock: effectiveBlock
+        });
+      }
 
     workers[maxThread - 1].postMessage({
+      type: "first",
       blockLength: blockLength + tail,
       costs: costs,
-      minSeed: i,
+      minSeed: i || 0,
       effectiveBlock: effectiveBlock
     });
   });
 }
 
+var aaa=0;
+function addInput(costs, effectiveBlock) {
+  return new Promise(function (res, rej) {
+    //console.log(average);
+    var completed = 0
+      , average_ = 0
+      , seeds = [];
+    workers.forEach(function (a, b) {
+      a.addEventListener("message", function (e) {
+        if (completed >= maxThread)
+          return;
+        seeds = seeds.concat(e.data.seeds)
+        completed++;
+        average_ += e.data.t;
+        if (completed >= maxThread) {
+          average_ /= maxThread;
+          average += average_;
+          //console.log(average, aaa++);
+          res({
+            seeds: seeds,
+            avrTime: average
+          })
+        }
+      }, !1)
+    });
+
+    for (var j = 0; j < maxThread; j++) {
+      workers[j].postMessage({
+        costs: costs,
+        effectiveBlock: effectiveBlock
+      });
+    }
+  });
+}
+
 async function main() {
-  console.log(await firstInput([8, 17, 28], 14))
+  firstInput([3, 8, 30], 15)
+  .then(_ => addInput([6, 14, 28], 14))
+  .then(_ => addInput([5, 11, 26], 13))
+  .then(_ => addInput([6, 14, 24], 12))
+  .then(_ => addInput([3, 7, 22], 11))
+  .then(_ => addInput([5, 11, 20], 10))
+  .then(_ => addInput([4, 9, 18], 9))
+  .then(_ => addInput([4, 10, 16], 8))
+  .then(_ => addInput([2, 5, 14], 7))
+  .then(_ =>addInput([4, 9, 13], 6))
+  .then(_ =>addInput([2, 5, 10], 5))
+  .then(_ =>addInput([3, 7, 10], 4))
+  .then(_ =>addInput([1, 4, 6], 3))
+  .then(_ =>addInput([1, 4, 5], 2))
+  .then(_ => console.log(_));
 }
 
 main();
--- Enchantment/EnchantmentSeedCracker/Worker.js
+++ Enchantment/EnchantmentSeedCracker/Worker.js
@@ -49,6 +49,8 @@ async function calcAll(data) {
   return seeds*/
 }
 
+var seeds = [], nextSeeds = [];
+
 self.addEventListener("message", function (d) {
   var data = d.data
     , t0 = new Date();
@@ -61,30 +63,58 @@ self.addEventListener("message", function (d) {
     , cost2 = data.costs[1]
     , cost3 = data.costs[2]
     , block = data.effectiveBlock
-    , seeds = []
-    , random = new WebAssembly.Instance(
+    /*,random = new WebAssembly.Instance(
       new WebAssembly.Module(
         //new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 13, 2, 96, 0, 1, 127, 96, 4, 127, 127, 127, 127, 1, 127, 3, 7, 6, 0, 1, 1, 1, 1, 1, 6, 6, 1, 127, 1, 65, 0, 11, 7, 50, 6, 3, 109, 117, 108, 0, 1, 5, 100, 105, 118, 95, 115, 0, 2, 5, 100, 105, 118, 95, 117, 0, 3, 5, 114, 101, 109, 95, 115, 0, 4, 5, 114, 101, 109, 95, 117, 0, 5, 8, 103, 101, 116, 95, 104, 105, 103, 104, 0, 0, 10, 191, 1, 6, 4, 0, 35, 0, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 126, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 127, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 128, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 129, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11, 36, 1, 1, 126, 32, 0, 173, 32, 1, 173, 66, 32, 134, 132, 32, 2, 173, 32, 3, 173, 66, 32, 134, 132, 130, 34, 4, 66, 32, 135, 167, 36, 0, 32, 4, 167, 11])
         fs.readFileSync(pl.join(__dirname, "./SimplifiedMT.wasm"))
       ), {}
-    ).exports
+    ).exports*/
     , random = new SimplifiedMT(0)
     //, random = new MT(0);
 
-  for (var i = data.minSeed; i < end; i++) {
-    random.setSeed(i);
-    var v0 = random.nextInt(8)
-      , v1 = random.nextInt(block + 1)
-      , v2 = v0 + v1 + (block >> 1) + 1;
+  if (data.type == "first") {
+    //console.log(data, end);
+    seeds = [];
+    nextSeeds = [];
+    for (var i = data.minSeed; i < end; i++) {
+      //console.log(i)
+      random.setSeed(i);
+      var v0 = random.nextInt(8)
+        , v1 = random.nextInt(block + 1)
+        , v2 = v0 + v1 + (block >> 1) + 1;
 
-    if (cost1 != Math.max(Math.floor(v2 / 3), 1))
-      continue;
-    if (cost2 != Math.floor(2 * v2 / 3) + 1)
-      continue;
-    if (cost3 != Math.max(v2, 2 * block))
-      continue;
-    seeds.push(i);
+      if (cost1 != Math.max(Math.trunc(v2 / 3), 1))
+        continue;
+      if (cost2 != Math.trunc(2 * v2 / 3) + 1)
+        continue;
+      if (cost3 != Math.max(v2, 2 * block))
+        continue;
+      seeds.push(i);
+    }
+  } else {
+    nextSeeds = [];
+    for (var s of seeds) {
+      //console.log(s)
+      random.setSeed(s);
+      var v0 = random.nextInt(8)
+        , v1 = random.nextInt(block + 1)
+        , v2 = v0 + v1 + (block >> 1) + 1;
+
+      if (cost1 != Math.max(Math.trunc(v2 / 3), 1))
+        continue;
+      if (cost2 != Math.trunc(2 * v2 / 3) + 1)
+        continue;
+      if (cost3 != Math.max(v2, 2 * block))
+        continue;
+      nextSeeds.push(s);
+    }
+    seeds = nextSeeds;
+   // console.log(seeds.length)
   }
 
-  self.postMessage({ seeds: seeds, t: (new Date()) - t0 })
+  self.postMessage({
+    type: "result",
+    seeds: seeds,
+    t: (new Date()) - t0
+  })
 });
